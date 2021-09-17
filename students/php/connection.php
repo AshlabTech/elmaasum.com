2d8666f7a41882c4d0e2d9093a4428b289d1d2df
Said Abdulsalam <42839808+saidabdul80@users.noreply.github.com>
2021-09-17 18:20:11 +0100

    update

---
+++ students/php/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$servername = "localhost";
+/* $username = "elmaasu1_user";
+$password = "_DU=yAUqdfuV";
+ */
+
+$username = "root";
+$password = "";
+
+$db = "elmaasu1_db";
+	//create login connection and login
+	$conn =  mysqli_connect($servername,$username,$password,$db) or die(mysqli_error($conn));
+	error_reporting(0);
+		$Fschool = mysqli_query($conn,"SELECT * FROM school LIMIT 1") or die(mysqli_error($conn));
+	$school = mysqli_fetch_array($Fschool);
+	$school_name = $school['name'];
+	$school_address = $school['address'];
+	$school_abbr = $school['abbr'];
+	$school_image = $school['image'];
+	
+?>
